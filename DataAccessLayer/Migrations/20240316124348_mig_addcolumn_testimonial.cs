a1d03f8cfe0008871c433a18ab8faf8618a33838
kaank <kaank@KAAN>
2024-03-17 12:50:06 +0300

    Testimonial

---
+++ DataAccessLayer/Migrations/20240316124348_mig_addcolumn_testimonial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_addcolumn_testimonial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectUrl",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "İmageUrl2",
+                table: "Portfolios");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Testimonials",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Testimonials");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "İmageUrl2",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
