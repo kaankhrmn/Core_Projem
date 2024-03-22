4b582316b1821b2702f0e72efcba541312baac61
kaank <kaank@KAAN>
2024-03-23 00:12:09 +0300

    Yetenekler

--- Core_Proje/Controllers/AdminController.cs
+++ Core_Proje/Controllers/AdminController.cs
@@ -4,9 +4,30 @@ namespace Core_Proje.Controllers
 {
 	public class AdminController : Controller
 	{
-		public PartialViewResult PartialSideBar()
+		public PartialViewResult PartialSideBar() //sol menü burda takılacak
 		{
 			return PartialView();
 		}
-	}
+       
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavigationPartial()
+        {
+            return PartialView();
+        }
+    }
 }
--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
@@ -6,7 +9,51 @@ namespace Core_Proje.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
+
+            var values = skillManager.GetList();
+            return View(values);
+            
+        }
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+       
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index"); //index aksiyonuna yönlendirir.
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetById(id);
+            skillManager.TDelete(values); //silme işlemini yapmak için ilk önce o nesneyi bulmam lazım.
+            return RedirectToAction("Index");
+        }
+        [HttpGet] //sayfa yüklendiği zaman çalışır
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = skillManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
