43e409422cabac4ed14783f7fea1ee97139e6b0f
kaank <kaank@KAAN>
2024-03-25 16:23:37 +0300

    Portfolio

---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+	public class PortfolioController : Controller
+	{
+		PortfolioManager portfolioManager =new PortfolioManager(new EfPortfolioDal());
+
+		public IActionResult Index()
+		{
+			ViewBag.v1 = "Portfolio Düzenlemesi";
+            ViewBag.v2 = "Portfolio";
+            ViewBag.v3 = "Portfolio Düzenlemesi";
+            var values = portfolioManager.GetList();
+			return View(values);
+		}
+	}
+}
